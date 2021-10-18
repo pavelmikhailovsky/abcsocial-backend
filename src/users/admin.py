d8f644417e727742d05f75369698228e444b464e
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-10-18 09:24:29 +0300

    init project, created elementary app users and settings for custom user model

---
+++ src/users/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from .models import User
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # fieldsets = UserAdmin.fieldsets + (
+    #     (None, {'fields': ('status', 'image',)}),
+    # )
+    # add_fieldsets = UserAdmin.add_fieldsets + (
+    #     (None, {'fields': ('status', 'image', 'first_name', 'last_name')}),
+    # )
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
